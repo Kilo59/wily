ef44606126ed98d4742a591f24b44157a394a623
Gabriel <gabriel59kg@gmail.com>
2019-01-26 19:59:59 -0500

    module discovery function

--- test/unit/test_basic.py
+++ test/unit/test_basic.py
@@ -1,3 +1,23 @@
 import pathlib
+from pkgutil import iter_modules
+from setuptools import find_packages
+# import pytest
 
-import pytest
+WILY_ROOT = pathlib.Path("wily").resolve()
+print(WILY_ROOT)
+
+def get_all_modules():
+    wily_modules = set()
+    for pkg in find_packages():
+        pkg_path = f"{WILY_ROOT.parent}/{pkg.replace('.', '/')}"
+        print(pkg_path, pkg)
+        for _, name, ispkg in iter_modules([pkg_path]):
+            if ispkg:
+                pass
+            else:
+                # print("added", name)
+                wily_modules.add(f"{pkg}.{name}")
+    return wily_modules
+
+if __name__ == "__main__":
+    print(get_all_modules())
